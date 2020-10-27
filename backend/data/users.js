b6d4724150b365f1dcfa5b7804995bf1273d9835
Ismail Khan <ismailislamkhan@gmail.com>
2020-10-26 21:02:39 -0400

    setup bcrypt and hash user passwords

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+// an array of users equating to our data model for Users
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+]
+
+export default Users
